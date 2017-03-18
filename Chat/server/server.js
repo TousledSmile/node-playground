f344112d4ca42b7045a24721f4295be2c797763c
TousledSmile <anita.wlosek@gmail.com>
2017-03-19 00:47:10 +0100

    server.js - 404

--- Chat/server/server.js
+++ Chat/server/server.js
@@ -4,3 +4,12 @@ var path = require('path');
 var mime = require('mime');
 
 var cache = {};
+
+function send404(response) {
+    var headers = {
+        "Content-Type": 'text/plain'
+    }
+    response.writeHead(404, '', headers);
+    response.write('Error 404: no file');
+    response.end();
+}
